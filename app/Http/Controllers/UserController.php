1aab7cd6222cf18716827ae1f6066055e4b7e554
Andrés Zamora <andreszamora1998@gmail.com>
2019-04-22 12:00:07 -0500

    Decodificando json

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,9 @@ class UserController extends Controller
 	public function register(Request $request){
 
 		//Recoger los Datos POST
-
+			$json= $request->input('json',null);
+			$params=json_decode($json);//objeto
+			$paramsArray=json_decode($json,true);//array
 		//Validar Datos
 
 		//Cifrar Pass
