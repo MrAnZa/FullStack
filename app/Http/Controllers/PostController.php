54f9e9891825aa6d8df3177bcc5d7da5e6dfdfb1
Andrés Zamora <andreszamora1998@gmail.com>
2019-08-07 22:23:50 -0500

    Guardar Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Post;
+use App\Helpers\JwtAuth;
 
 class PostController extends Controller
 {
@@ -34,12 +35,65 @@ class PostController extends Controller
 				'post' => $post
 			];
 		}else {
-			[
+			$data=  [
 				'code' => 404,
 				'status' => 'error',
 				'message' => 'la Entrada no existe'
 			];
 		}
+		return response()->json($data, $data['code']);
+	}
+	public function store(Request $request) {
+		//Recoger datos por POST
+		$json = $request->input('json',null);
+		$params = json_decode($json);
+		$params_array = json_decode($json,true);
+
+		if(!empty($params_array)){
+		//Conseguir usuario identificado
+			$jwtAuth = new JwtAuth();
+			$token = $request->header('Authorization',null);
+			$user = $jwtAuth->checkToken($token,true);
+		//Validar datos
+			$validate = \Validator::make($params_array,[
+				'title' => 'required',
+				'content' => 'required',
+				'category_id' => 'required',
+				'image' => 'required'
+				
+			]);
+			if ($validate->fails()) {
+				$data=  [
+					'code' => 400,
+					'status' => 'error',
+					'message' => 'No se ha Guardado, faltan datos'
+				];
+			}else {
+				//Guardar el articulo
+				$post = new Post();
+				$post->user_id=$user->sub;
+				$post->category_id=$params->category_id;
+				$post->title=$params->title;
+				$post->content=$params->content;
+				$post->image=$params->image;
+				$post->save();
+				
+				$data=  [
+					'code' => 200,
+					'status' => 'success',
+					'post' => $post
+				];
+
+			}
+		
+		}else{
+			$data=  [
+				'code' => 400,
+				'status' => 'error',
+				'message' => 'envia los datos correctamente'
+			];
+		}
+		//Devolver respuesta
 		return response()->json($data, $data['code']);
 	}
 	
